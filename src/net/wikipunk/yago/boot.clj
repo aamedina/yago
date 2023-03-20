77105b035d745d2eb7c546205c3d230d779e3bf0
Adrian Medina <adrian@wikipunk.net>
2023-03-20 14:17:47 -0400

    add OWL 2 DL subset of schema.org from YAGO

--- src/net/wikipunk/yago/boot.clj
+++ src/net/wikipunk/yago/boot.clj
@@ -6,3 +6,14 @@
    :rdfa/uri    "http://yago-knowledge.org/resource/"
    :rdfa/prefix "yago"})
 
+(def schema
+  {:dcat/downloadURL "resources/yago-wd-schema.nt.gz"
+   :rdf/type         :rdfa/PrefixMapping
+   :rdfa/uri         "http://schema.org/"
+   :rdfa/prefix      "schema"})
+
+(def bioschema
+  {:rdf/type    :rdfa/PrefixMapping
+   :rdfa/uri    "http://bioschemas.org/"
+   :rdfa/prefix "bioschema"})
+
