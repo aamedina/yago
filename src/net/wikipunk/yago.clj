648c3e776324b37b3508ae5bee708cf3b7b2dee5
Adrian Medina <adrian@wikipunk.net>
2023-03-21 11:39:53 -0400

    add additional requires to component

--- src/net/wikipunk/yago.clj
+++ src/net/wikipunk/yago.clj
@@ -7,8 +7,11 @@
 (defrecord YAGO []
   com/Lifecycle
   (start [this]
+    (require 'net.wikipunk.rdf.schema :reload)
+    (require 'net.wikipunk.rdf.bioschema)
     (require 'net.wikipunk.rdf.yago)
     (require 'net.wikipunk.rdf.yago.full-facts)
+    (require 'net.wikipunk.rdf.yago.shapes)
     this)
   (stop [this]
     this))
